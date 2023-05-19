99a4e8063c15aebd5bd0db69859925d3baf673e3
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-05-19 16:36:20 +0200

    feat: add info to head component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,17 @@
+import theme from '@/styles/theme';
 import Head from 'next/head';
 
 const Header = () => {
   return (
     <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content={theme.colors.background} />
+      <meta
+        name="description"
+        content="Olympic Games App, you can see all your favorite athletes and olympic games here"
+      ></meta>
+      <link rel="icon" sizes="96x96" href="/favicon-96x96.png" />
       <title>Olympic Games App</title>
     </Head>
   );
